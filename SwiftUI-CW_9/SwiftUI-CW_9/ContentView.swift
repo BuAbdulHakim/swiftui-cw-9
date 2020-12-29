f343b080e68c65b0a1edf58d8c00a2fe21792f81
Nasser Alsaqabi <alsokor4@gmail.com>
2020-12-29 21:57:51 +0300

    init commit

---
+++ SwiftUI-CW_9/SwiftUI-CW_9/ContentView.swift
@@ -0,0 +1,51 @@
+//
+//  ContentView.swift
+//  SwiftUI-CW_9
+//
+//  Created by NasserAlsaqabi on 29/12/2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    var body: some View {
+        VStack{
+        Text("Movies")
+            .font(.largeTitle)
+        List{
+            MovieRow(movieName: "Toy_Story", movieCharacters: ["Woody", "Buzz"])
+            MovieRow(movieName: "Cars", movieCharacters: ["Lightning McQueen", "Mater", "Doc Hudson"])
+            MovieRow(movieName: "The_Karate_Kid", movieCharacters: ["Daniel LaRusso", "Mr. Miyagi", "Johnny Lawrence"])
+
+        }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
+struct MovieRow: View {
+    let movieName:String
+    let movieCharacters:[String]
+    
+    var body: some View {
+        HStack(alignment: .center){
+            Image(movieName)
+                .resizable()
+                .scaledToFit()
+                .frame(height: 100)
+                .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
+            
+            VStack(alignment: .leading){
+                Text(movieName)
+                    .font(.largeTitle)
+                Text(movieCharacters.joined(separator: ", "))
+            }
+        }.padding(.vertical)
+    }
+}
---
+++ SwiftUI-CW_9/SwiftUI-CW_9/SwiftUI_CW_9App.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_CW_9App.swift
+//  SwiftUI-CW_9
+//
+//  Created by NasserAlsaqabi on 29/12/2020.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_CW_9App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
